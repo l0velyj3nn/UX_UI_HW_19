3e79430fd60351e8760e4d2009df64c25d7f9dd0
l0velyj3nn <128440011+l0velyj3nn@users.noreply.github.com>
2023-04-29 17:13:23 -0700

    jQuery strong!
    Added fade in of about me statement

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Tap, tap, tap... is this thing on?");
+
+$(document).ready(function(){
+
+    /*! Fades in "about box" on load */
+    $('.content-intro-box.letsfadein').css('display', 'none');
+    $('.content-intro-box.letsfadein').fadeIn(1000);
+    });
