5fc8ac333cbb1469bd4c5ffc7d3bc2a1791f20c5
l0velyj3nn <128440011+l0velyj3nn@users.noreply.github.com>
2023-04-29 19:42:19 -0700

    Clippy is here
    Created a slow reveal of a Clippy character saying hello and inviting visitor to connect

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ $(document).ready(function(){
     $('.content-intro-box.letsfadein').css('display', 'none');
     $('.content-intro-box.letsfadein').fadeIn(1000);
     });
+
+    /* scroll and reveal */
+    var revealElement = $('section .reveal');
+$(window).scroll(function() {
+if($(window).scrollTop() + $(window).height() > $(document).height() - 1000) 
+
+{       $(revealElement).addClass('show');
+   } else if($(revealElement).hasClass('show') && $(window).scrollTop() + $(window).height() > $(document).height() - 1115) {
+      $(revealElement).removeClass('show');
+   }
+});
